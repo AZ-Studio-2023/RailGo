bdcaf0b9a5f310048ad2e3711518490da66f50dc
mstouk57g <KissLmisu@outlook.com>
2024-08-14 20:28:37 +0800

    将逻辑代码扔进来了（没MVVM）

---
+++ HCM.Core/Models/ListCollections.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json.Converters;
+using Newtonsoft.Json;
+
+namespace HCM.Core.Models;
+public class TrainInfo
+{
+    [JsonProperty("emu_no")]
+    public string 车组号
+    {
+        get; set;
+    }
+    [JsonProperty("train_no")]
+    public string 担当车次
+    {
+        get; set;
+    }
+    [JsonProperty("date")]
+    [JsonConverter(typeof(CustomDateTimeConverter))]
+    public DateTime 日期
+    {
+        get; set;
+    }
+}
+public class CustomDateTimeConverter : IsoDateTimeConverter
+{
+    public CustomDateTimeConverter()
+    {
+        DateTimeFormat = "yyyy-MM-dd HH:mm";
+    }
+}
--- HCM/Views/EMU_RoutingPage.xaml.cs
+++ HCM/Views/EMU_RoutingPage.xaml.cs
@@ -1,6 +1,12 @@
-﻿using HCM.ViewModels;
+﻿using HCM.Core.Models;
+using HCM.ViewModels;
 
 using Microsoft.UI.Xaml.Controls;
+using Newtonsoft.Json;
+using Microsoft.UI.Xaml;
+using System.Diagnostics;
+using Windows.ApplicationModel.Resources;
+using Microsoft.Windows.ApplicationModel.Resources;
 
 namespace HCM.Views;
 
@@ -15,5 +21,50 @@ public sealed partial class EMU_RoutingPage : Page
     {
         ViewModel = App.GetService<EMU_RoutingViewModel>();
         InitializeComponent();
+    }
+
+    // 这一部分没有遵循MVVM设计模式
+    // 不管了，马上开学了，能跑就行（
+    // 会抽空重构这个破玩意的
+    public string content;
+    public string url;
+    public async Task GetContent()
+    {
+        try
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                HttpResponseMessage response = await client.GetAsync(url);
+                if (response.IsSuccessStatusCode)
+                {
+                    content = await response.Content.ReadAsStringAsync();
+                }
+                else
+                {
+                    Button_Click();
+                }
+            }
+            List<TrainInfo> trainInfos = JsonConvert.DeserializeObject<List<TrainInfo>>(content);
+            dataGrid.ItemsSource = trainInfos;
+        }
+        catch
+        {
+            Button_Click();
+        }
+    }
+    private void GetEmuBtn_Click(object sender, RoutedEventArgs e)
+    {
+        if (EmuIdTextBox.Text != null)
+        {
+            url = "https://api.rail.re/emu/" + EmuIdTextBox.Text;
+            GetContent();
+        }
+
+    }
+    
+    //弹窗
+    private void Button_Click()
+    {
+        NotificationQueue.Show(null, 2000);
     }
 }
