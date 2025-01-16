e1b26c597356d2a64a67c681075bdf5db817dd65
mstouk57g <KissLmisu@outlook.com>
2025-01-16 08:59:15 +0800

    简化已有绑定代码

--- RailGo/ViewModels/EMU_RoutingViewModel.cs
+++ RailGo/ViewModels/EMU_RoutingViewModel.cs
@@ -12,25 +12,10 @@ namespace RailGo.ViewModels;
 
 public partial class EMU_RoutingViewModel : ObservableObject
 {
-    ObservableCollection<TrainTripsInfo> _TrainNumberEmuInfo = new ObservableCollection<TrainTripsInfo>();
-    public ObservableCollection<TrainTripsInfo> trainNumberEmuInfos
-    {
+    [ObservableProperty]
+    public ObservableCollection<TrainTripsInfo> trainNumberEmuInfos;
 
-        get
-        {
-            return _TrainNumberEmuInfo;
-        }
-        set
-        {
-            _TrainNumberEmuInfo = value;
-            OnPropertyChanged("TrainTripsInfo");
-        }
-    }
-
-    public string EmuRoatingcontent;
     public string InputEmuID;
-    public string url = "https://api.rail.re/emu/";
-    public string XGZurl = "http://www.xiaguanzhan.com/";
     public MainWindowViewModel progressBarVM = App.GetService<MainWindowViewModel>();
 
     public EMU_RoutingViewModel()
@@ -39,33 +24,13 @@ public partial class EMU_RoutingViewModel : ObservableObject
     public async Task GettrainNumberEmuInfosContent()
     {
         progressBarVM.TaskIsInProgress = "Visible";
-        try
-        {
-            using (HttpClient client = new HttpClient())
-            {
-                HttpResponseMessage response = await client.GetAsync(url + InputEmuID);
-                if (response.IsSuccessStatusCode)
-                {
-                    EmuRoatingcontent = await response.Content.ReadAsStringAsync();
-                    XGZurl = "http://www.xiaguanzhan.com/soso.asp?keyword=" + InputEmuID;
-                }
-                else
-                {
-                    // NotificationQueue.Show(null, 2000);
-                }
-
-            }
-            var newTrainInfos = JsonConvert.DeserializeObject<ObservableCollection<TrainTripsInfo>>(EmuRoatingcontent);
-            trainNumberEmuInfos.Clear();
-            foreach (var trainInfo in newTrainInfos)
-            {
-                trainNumberEmuInfos.Add(trainInfo);
-            }
-        }
-        catch
-        {
-            // NotificationQueue.Show(null, 2000);
-        }
+        var httpClient = new HttpClient();
+        var requestMessage = new HttpRequestMessage(HttpMethod.Get, "https://api.rail.re/emu/" + InputEmuID);
+        var response = await httpClient.SendAsync(requestMessage);
+        var data = await response.Content.ReadAsStringAsync();
+        var newTrainInfos = JsonConvert.DeserializeObject<ObservableCollection<TrainTripsInfo>>(data);
+        TrainNumberEmuInfos = newTrainInfos;
+        Trace.WriteLine(data);
         progressBarVM.TaskIsInProgress = "Collapsed";
     }
 }
--- RailGo/ViewModels/Train_NumberViewModel.cs
+++ RailGo/ViewModels/Train_NumberViewModel.cs
@@ -13,56 +13,22 @@ public partial class Train_NumberViewModel : ObservableRecipient
     public Train_NumberViewModel()
     {
     }
-    ObservableCollection<TrainTripsInfo> _TrainNumberTripsInfo = new ObservableCollection<TrainTripsInfo>();
-    public ObservableCollection<TrainTripsInfo> trainNumberTripsInfos
-    {
 
-        get
-        {
-            return _TrainNumberTripsInfo;
-        }
-        set
-        {
-            _TrainNumberTripsInfo = value;
-            OnPropertyChanged("trainNumberTripsInfos");
-        }
-    }
+    [ObservableProperty]
+    public ObservableCollection<TrainTripsInfo> trainNumberTripsInfos;
 
-    private static DateTime nowdateTime = DateTime.Now;
-    public string TrainTripscontent;
     public string InputTrainTrips;
-    public string url = "https://api.rail.re/train/";
     public MainWindowViewModel progressBarVM = App.GetService<MainWindowViewModel>();
 
     public async Task GettrainNumberTripsInfosContent()
     {
         progressBarVM.TaskIsInProgress = "Visible";
-        try
-        {
-            using (HttpClient client = new HttpClient())
-            {
-                HttpResponseMessage response = await client.GetAsync(url + InputTrainTrips);
-                if (response.IsSuccessStatusCode)
-                {
-                    TrainTripscontent = await response.Content.ReadAsStringAsync();
-                }
-                else
-                {
-                    // NotificationQueue.Show(null, 2000);
-                }
-
-            }
-            var newTrainInfos = JsonConvert.DeserializeObject<ObservableCollection<TrainTripsInfo>>(TrainTripscontent);
-            trainNumberTripsInfos.Clear();
-            foreach (var trainInfo in newTrainInfos)
-            {
-                trainNumberTripsInfos.Add(trainInfo);
-            }
-        }
-        catch
-        {
-            // NotificationQueue.Show(null, 2000);
-        }
+        var httpClient = new HttpClient();
+        var requestMessage = new HttpRequestMessage(HttpMethod.Get, "https://api.rail.re/train/" + InputTrainTrips);
+        var response = await httpClient.SendAsync(requestMessage);
+        var data = await response.Content.ReadAsStringAsync();
+        var newTrainInfos = JsonConvert.DeserializeObject<ObservableCollection<TrainTripsInfo>>(data);
+        TrainNumberTripsInfos = newTrainInfos;
         progressBarVM.TaskIsInProgress = "Collapsed";
     }
 }
