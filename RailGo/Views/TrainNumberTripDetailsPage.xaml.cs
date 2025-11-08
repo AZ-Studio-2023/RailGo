5ba8839aec0b2fd995a7658e1d9c23188c607b95
mstouk57g <KissLmisu@outlook.com>
2025-11-08 18:27:11 +0800

    车次信息中添加跳转到对应车站按钮

--- RailGo/Views/TrainNumberTripDetailsPage.xaml.cs
+++ RailGo/Views/TrainNumberTripDetailsPage.xaml.cs
@@ -13,6 +13,7 @@ namespace RailGo.Views;
 public sealed partial class TrainNumberTripDetailsPage : Page
 {
     public TrainPreselectResult DataFromLast => DataContext as TrainPreselectResult;
+    public TimetableItem _item_ViaStaion;
     public TrainNumberTripDetailsViewModel ViewModel
     {
         get;
@@ -32,5 +33,50 @@ public sealed partial class TrainNumberTripDetailsPage : Page
         date = System.DateTime.Now.ToString("yyyyMMdd");
         ViewModel.GetInformationCommand.Execute((train_no, date));
         this.Loaded -= OnLoad;
+    }
+    private async void DetailBtnClick(object sender, RoutedEventArgs e)
+    {
+        if (sender is HyperlinkButton button)
+        {
+            string BarHeader = null;
+            string icon = null;
+            Page page = null;
+            StationTrain Details = null;
+            List<string> StationType = new() { "SearchingST" };
+            bool Await = false;
+            // 根据选择的导航按钮切换右侧内容
+            switch (button.Name.ToString())
+            {
+                case "ViaStationsStationInformation":
+                    icon = "\uF161";
+                    BarHeader = _item_ViaStaion.Station;
+                    page = new StationDetailsPage()
+                    {
+                        DataContext = new StationPreselectResult { Name = _item_ViaStaion.Station, TeleCode = _item_ViaStaion.StationTelecode, Type = StationType }
+                    };
+                    break;
+            }
+
+            TabViewItem tabViewItem = new()
+            {
+                Header = BarHeader,
+                Content = page,
+                CanDrag = true,
+                IconSource = new FontIconSource() { Glyph = icon }
+            };
+            MainWindow.Instance.MainTabView.TabItems.Add(tabViewItem);
+            MainWindow.Instance.MainTabView.SelectedItem = tabViewItem;
+
+            if (Await)
+            {
+
+                //var StationDetails = await ApiService.StationPreselectAsync(BarHeader);
+                //page = new StationDetailsPage()
+                // {
+                // DataContext = new StationPreselectResult { Name = Details.ToStation.Station, TeleCode = Details.ToStation.StationTelecode, Type = StationDetails }
+                // };
+            }
+        }
+
     }
 }
