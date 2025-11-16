9db2e8bcab1a7dc832376974a19050fc14a790ae
mstouk57g <KissLmisu@outlook.com>
2025-11-16 18:24:47 +0800

    在主页上加了三个快速导航的按钮

--- RailGo/ViewModels/Pages/Shell/MainViewModel.cs
+++ RailGo/ViewModels/Pages/Shell/MainViewModel.cs
@@ -1,10 +1,40 @@
-﻿using CommunityToolkit.Mvvm.ComponentModel;
+﻿using System.Diagnostics;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using Microsoft.UI.Xaml.Controls;
+using RailGo.Core.Query;
+using RailGo.ViewModels.Pages.Stations;
+using RailGo.ViewModels.Pages.TrainEmus;
+using RailGo.ViewModels.Pages.Trains;
 
 namespace RailGo.ViewModels.Pages.Shell;
 
 public partial class MainViewModel : ObservableRecipient
 {
+    public Contracts.Services.INavigationService navigationService = App.GetService<Contracts.Services.INavigationService>();
     public MainViewModel()
     {
+    }
+
+    [RelayCommand]
+    private async Task NavigationAsync(object parameter)
+    {
+        string buttonName = parameter?.ToString() ?? string.Empty;
+
+        switch (buttonName)
+        {
+            case "ToTrainEmusButton":
+                navigationService.NavigateTo(typeof(EMU_RoutingViewModel).FullName!);
+                break;
+            case "ToTrainsButton":
+                navigationService.NavigateTo(typeof(Train_NumberViewModel).FullName!);
+                break;
+            case "ToStationsButton":
+                navigationService.NavigateTo(typeof(Station_InformationViewModel).FullName!);
+                break;
+            default:
+                navigationService.NavigateTo(typeof(MainViewModel).FullName!);
+                break;
+        }
     }
 }
