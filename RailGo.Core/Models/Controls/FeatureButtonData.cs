1d051c21122498b98ffd65601db100e5e9ce2239
mstouk57g <KissLmisu@outlook.com>
2025-11-19 00:30:32 +0800

    修改主页的三个导航按钮样式

---
+++ RailGo.Core/Models/Controls/FeatureButtonData.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RailGo.Core.Models.Controls;
+public class FeatureButtonData
+{
+    public string Icon
+    {
+        get; set;
+    }
+    public string Title
+    {
+        get; set;
+    }
+    public string Description
+    {
+        get; set;
+    }
+}
