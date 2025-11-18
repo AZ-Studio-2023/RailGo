8afa9ff91ae645ab13473a30f5c70bbcc13636cd
ntcho&ConiMite <KissLmisu@outlook.com>
2025-11-18 18:01:24 +0800

    [✨Feat] Add a MainPage (1/?)

--- RailGo/ViewModels/Pages/Shell/MainViewModel.cs
+++ RailGo/ViewModels/Pages/Shell/MainViewModel.cs
@@ -1,10 +1,128 @@
-﻿using CommunityToolkit.Mvvm.ComponentModel;
+﻿using System.Collections.ObjectModel;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using Microsoft.UI.Dispatching;
+using Microsoft.UI.Xaml;
+using RailGo.Core.Query.Online;
+using RailGo.ViewModels.Pages.Stations;
+using RailGo.ViewModels.Pages.TrainEmus;
+using RailGo.ViewModels.Pages.Trains;
 
 namespace RailGo.ViewModels.Pages.Shell;
 
-public partial class MainViewModel : ObservableRecipient
+public partial class MainViewModel : ObservableObject
 {
+    public Contracts.Services.INavigationService navigationService = App.GetService<Contracts.Services.INavigationService>();
+
+    [ObservableProperty]
+    private ObservableCollection<string> _bannerImages = new ObservableCollection<string>();
+
+    [ObservableProperty]
+    private int _currentBannerIndex = 0;
+
+    [ObservableProperty]
+    private bool _isAutoPlayEnabled = true;
+
+    private DispatcherTimer _autoPlayTimer;
+
     public MainViewModel()
     {
+        _ = LoadBannerImagesAsync();
+        InitializeAutoPlayTimer();
+    }
+
+    private void InitializeAutoPlayTimer()
+    {
+        _autoPlayTimer = new DispatcherTimer();
+        _autoPlayTimer.Interval = TimeSpan.FromSeconds(5);
+        _autoPlayTimer.Tick += AutoPlayTimer_Tick;
+
+        if (_isAutoPlayEnabled)
+        {
+            _autoPlayTimer.Start();
+        }
+    }
+
+    private void AutoPlayTimer_Tick(object sender, object e)
+    {
+        if (BannerImages.Count == 0) return;
+
+        CurrentBannerIndex = (CurrentBannerIndex + 1) % BannerImages.Count;
+    }
+
+    [RelayCommand]
+    private void BannerSelectionChanged(int selectedIndex)
+    {
+        if (selectedIndex >= 0 && selectedIndex < BannerImages.Count)
+        {
+            CurrentBannerIndex = selectedIndex;
+            ResetAutoPlayTimer();
+        }
+    }
+
+    private void ResetAutoPlayTimer()
+    {
+        _autoPlayTimer?.Stop();
+        _autoPlayTimer?.Start();
+    }
+
+    public void PauseAutoPlay()
+    {
+        _autoPlayTimer?.Stop();
+    }
+
+    public void ResumeAutoPlay()
+    {
+        if (_isAutoPlayEnabled && _autoPlayTimer != null && !_autoPlayTimer.IsEnabled)
+        {
+            _autoPlayTimer.Start();
+        }
+    }
+
+    private async Task LoadBannerImagesAsync()
+    {
+        try
+        {
+            var images = await SettingsAPIService.GetBannerImagesAsync();
+
+            if (images?.Count > 0)
+            {
+                BannerImages.Add("ms-appx:///Assets/AutoBanner.png");
+                foreach (var imageUrl in images)
+                {
+                    BannerImages.Add(imageUrl);
+                }
+
+                if (_isAutoPlayEnabled && _autoPlayTimer != null && !_autoPlayTimer.IsEnabled)
+                {
+                    _autoPlayTimer.Start();
+                }
+            }
+        }
+        catch
+        {
+        }
+    }
+
+    [RelayCommand]
+    private async Task NavigationAsync(object parameter)
+    {
+        string buttonName = parameter?.ToString() ?? string.Empty;
+
+        switch (buttonName)
+        {
+            case "ToTrainEmusButton":
+                navigationService.NavigateTo(typeof(EMU_RoutingViewModel).FullName!);
+                break;
+            case "ToTrainsButton":
+                navigationService.NavigateTo(typeof(Train_NumberViewModel).FullName!);
+                break;
+            case "ToStationsButton":
+                navigationService.NavigateTo(typeof(Station_InformationViewModel).FullName!);
+                break;
+            default:
+                navigationService.NavigateTo(typeof(MainViewModel).FullName!);
+                break;
+        }
     }
 }
--- RailGo/Views/Pages/Shell/MainPage.xaml.cs
+++ RailGo/Views/Pages/Shell/MainPage.xaml.cs
@@ -1,11 +1,14 @@
-﻿using RailGo.ViewModels.Pages.Shell;
-
+﻿using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Media.Animation;
+using RailGo.ViewModels.Pages.Shell;
 
 namespace RailGo.Views.Pages.Shell;
 
 public sealed partial class MainPage : Page
 {
+    private const double BannerWidthRatio = 1938;
+    private const double BannerHeightRatio = 585;
     public MainViewModel ViewModel
     {
         get;
@@ -15,5 +18,49 @@ public sealed partial class MainPage : Page
     {
         ViewModel = App.GetService<MainViewModel>();
         InitializeComponent();
+
+        ViewModel.PropertyChanged += ViewModel_PropertyChanged;
+    }
+
+    private void ViewModel_PropertyChanged(object sender, System.ComponentModel.PropertyChangedEventArgs e)
+    {
+        if (e.PropertyName == nameof(ViewModel.CurrentBannerIndex))
+        {
+            if (BannerFlipView.SelectedIndex != ViewModel.CurrentBannerIndex)
+            {
+                BannerFlipView.SelectedIndex = ViewModel.CurrentBannerIndex;
+            }
+        }
+    }
+
+    private void Page_Loaded(object sender, RoutedEventArgs e)
+    {
+        UpdateBannerHeight();
+        ViewModel.ResumeAutoPlay();
+    }
+
+    private void Page_Unloaded(object sender, RoutedEventArgs e)
+    {
+        ViewModel.PauseAutoPlay();
+    }
+
+    private void BannerFlipView_SelectionChanged(object sender, SelectionChangedEventArgs e)
+    {
+        if (sender is FlipView flipView)
+        {
+            ViewModel.BannerSelectionChangedCommand?.Execute(flipView.SelectedIndex);
+        }
+    }
+
+    private void OnContentAreaSizeChanged(object sender, SizeChangedEventArgs e)
+    {
+        UpdateBannerHeight();
+    }
+
+    private void UpdateBannerHeight()
+    {
+        double pageWidth = ContentArea.ActualWidth;
+        double newHeight = (pageWidth / BannerWidthRatio) * BannerHeightRatio;
+        BannerFlipView.Height = newHeight;
     }
 }
