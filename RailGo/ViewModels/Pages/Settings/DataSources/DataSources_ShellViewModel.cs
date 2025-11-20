ef18aabbf000ad5e829cf795c1a2579925e84f2e
mstouk57g <KissLmisu@outlook.com>
2025-11-20 17:44:30 +0800

    删掉DataSourecesSettingsViewModels中没用的方法

--- RailGo/ViewModels/Pages/Settings/DataSources/DataSources_ShellViewModel.cs
+++ RailGo/ViewModels/Pages/Settings/DataSources/DataSources_ShellViewModel.cs
@@ -15,51 +15,8 @@ namespace RailGo.ViewModels.Pages.Settings.DataSources;
 
 public partial class DataSources_ShellViewModel : ObservableRecipient
 {
-    private readonly IThemeSelectorService _themeSelectorService;
-
-    [ObservableProperty]
-    private ElementTheme _elementTheme;
-
-    [ObservableProperty]
-    private string _versionDescription;
-
-    public ICommand SwitchThemeCommand
-    {
-        get;
-    }
-
     public DataSources_ShellViewModel(IThemeSelectorService themeSelectorService)
     {
-        _themeSelectorService = themeSelectorService;
-        _elementTheme = _themeSelectorService.Theme;
-        _versionDescription = GetVersionDescription();
-
-        SwitchThemeCommand = new RelayCommand<ElementTheme>(
-            async (param) =>
-            {
-                if (ElementTheme != param)
-                {
-                    ElementTheme = param;
-                    await _themeSelectorService.SetThemeAsync(param);
-                }
-            });
-    }
-
-    private static string GetVersionDescription()
-    {
-        Version version;
-
-        if (RuntimeHelper.IsMSIX)
-        {
-            var packageVersion = Package.Current.Id.Version;
-
-            version = new(packageVersion.Major, packageVersion.Minor, packageVersion.Build, packageVersion.Revision);
-        }
-        else
-        {
-            version = Assembly.GetExecutingAssembly().GetName().Version!;
-        }
-
-        return $"{"AppDisplayName".GetLocalized()} - {version.Major}.{version.Minor}.{version.Build}.{version.Revision}";
+        
     }
 }
