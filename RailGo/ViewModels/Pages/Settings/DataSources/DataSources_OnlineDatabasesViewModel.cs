e990bd6bd14bc6becca49121d0c10d76fe4a58eb
mstouk57g <KissLmisu@outlook.com>
2025-11-21 21:52:29 +0800

    添加最新数据库版本信息获取方法触发器

--- RailGo/ViewModels/Pages/Settings/DataSources/DataSources_OnlineDatabasesViewModel.cs
+++ RailGo/ViewModels/Pages/Settings/DataSources/DataSources_OnlineDatabasesViewModel.cs
@@ -10,6 +10,13 @@ using RailGo.Helpers;
 using Microsoft.UI.Xaml;
 
 using Windows.ApplicationModel;
+using Microsoft.UI.Xaml.Controls;
+using RailGo.Services;
+using RailGo.Core.Models.QueryDatas;
+using RailGo.ViewModels.Pages.Shell;
+using System.Collections.ObjectModel;
+using RailGo.Core.Query.Online;
+using RailGo.Core.Models.Settings;
 
 namespace RailGo.ViewModels.Pages.Settings.DataSources;
 
@@ -18,5 +25,29 @@ public partial class DataSources_OnlineDatabasesViewModel : ObservableRecipient
     public DataSources_OnlineDatabasesViewModel(IThemeSelectorService themeSelectorService)
     {
 
+    }
+    public MainWindowViewModel progressBarVM = App.GetService<MainWindowViewModel>();
+
+    [ObservableProperty]
+    private VersionInfo remoteDBInfo = new VersionInfo() { Db = "未获取" };
+
+
+    [RelayCommand]
+    private async Task GetRemoteDBInfoAsync()
+    {
+
+        try
+        {
+            progressBarVM.TaskIsInProgress = "Visible";
+            RemoteDBInfo = await DBGetService.GetVersionInfoAsync();
+
+        }
+        catch (Exception ex)
+        {
+        }
+        finally
+        {
+            progressBarVM.TaskIsInProgress = "Collapsed";
+        }
     }
 }
