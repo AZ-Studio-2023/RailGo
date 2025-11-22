14c51a4dc5e6949f7f8e1aff943cc11fe766129a
mstouk57g <KissLmisu@outlook.com>
2025-11-22 22:55:46 +0800

    添加本地数据库支持

--- RailGo/Views/Pages/Settings/DataSources/DataSources_LocalDatabasesPage.xaml.cs
+++ RailGo/Views/Pages/Settings/DataSources/DataSources_LocalDatabasesPage.xaml.cs
@@ -17,4 +17,50 @@ public sealed partial class DataSources_LocalDatabasesPage : Page
         ViewModel = App.GetService<DataSources_LocalDatabasesViewModel>();
         InitializeComponent();
     }
+
+    private async void AddButton_Click(object sender, RoutedEventArgs e)
+    {
+        var result = await AddDialog.ShowAsync();
+        if (result == ContentDialogResult.Primary)
+        {
+            ViewModel.AddLocalDatabaseSourceCommand.Execute(null);
+        }
+    }
+
+    private async void EditButton_Click(object sender, RoutedEventArgs e)
+    {
+        if (ViewModel.Item == null)
+        {
+            return;
+        }
+
+        ViewModel.ShowEditLocalDatabaseSourceCommand.Execute(null);
+        var result = await EditDialog.ShowAsync();
+        if (result == ContentDialogResult.Primary)
+        {
+            ViewModel.EditLocalDatabaseSourceCommand.Execute(null);
+        }
+    }
+
+    private async void DeleteButton_Click(object sender, RoutedEventArgs e)
+    {
+        if (ViewModel.Item == null)
+        {
+            return;
+        }
+
+        var result = await DeleteDialog.ShowAsync();
+        if (result == ContentDialogResult.Primary)
+        {
+            ViewModel.DeleteLocalDatabaseSourceCommand.Execute(null);
+        }
+    }
+    private async void ViewButton_Click(object sender, RoutedEventArgs e)
+    {
+        if (ViewModel.Item == null)
+        {
+            return;
+        }
+        var result = await ViewDialog.ShowAsync();
+    }
 }
