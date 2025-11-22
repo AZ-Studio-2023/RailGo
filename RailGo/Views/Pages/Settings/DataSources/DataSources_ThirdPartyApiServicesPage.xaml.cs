f44c77461c256f3c2625aefd28b6c4ad8a42d7f2
mstouk57g <KissLmisu@outlook.com>
2025-11-22 22:33:14 +0800

    添加本地第三方源设置

--- RailGo/Views/Pages/Settings/DataSources/DataSources_ThirdPartyApiServicesPage.xaml.cs
+++ RailGo/Views/Pages/Settings/DataSources/DataSources_ThirdPartyApiServicesPage.xaml.cs
@@ -1,7 +1,10 @@
 ﻿using System.Diagnostics;
 using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
+using RailGo.Core.Models.QueryDatas;
+using RailGo.Core.Models.Settings;
 using RailGo.ViewModels.Pages.Settings.DataSources;
+using RailGo.Services;
 
 namespace RailGo.Views.Pages.Settings.DataSources;
 
@@ -16,5 +19,51 @@ public sealed partial class DataSources_ThirdPartyApiServicesPage : Page
     {
         ViewModel = App.GetService<DataSources_ThirdPartyApiServicesViewModel>();
         InitializeComponent();
+    }
+
+    private async void AddButton_Click(object sender, RoutedEventArgs e)
+    {
+        var result = await AddDialog.ShowAsync();
+        if (result == ContentDialogResult.Primary)
+        {
+            ViewModel.AddOnlineApiSourceCommand.Execute(null);
+        }
+    }
+
+    private async void EditButton_Click(object sender, RoutedEventArgs e)
+    {
+        if (ViewModel.Item == null)
+        {
+            return;
+        }
+
+        ViewModel.ShowEditOnlineApiSourceCommand.Execute(null);
+        var result = await EditDialog.ShowAsync();
+        if (result == ContentDialogResult.Primary)
+        {
+            ViewModel.EditOnlineApiSourceCommand.Execute(null);
+        }
+    }
+
+    private async void DeleteButton_Click(object sender, RoutedEventArgs e)
+    {
+        if (ViewModel.Item == null)
+        {
+            return;
+        }
+
+        var result = await DeleteDialog.ShowAsync();
+        if (result == ContentDialogResult.Primary)
+        {
+            ViewModel.DeleteOnlineApiSourceCommand.Execute(null);
+        }
+    }
+    private async void ViewButton_Click(object sender, RoutedEventArgs e)
+    {
+        if (ViewModel.Item == null)
+        {
+            return;
+        }
+        var result = await ViewDialog.ShowAsync();
     }
 }
