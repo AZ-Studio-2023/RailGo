5be12c61ff7cd57b03942c4e1f3b68097a53d901
mstouk57g <KissLmisu@outlook.com>
2025-11-29 12:23:44 +0800

    修复默认url对齐问题

--- RailGo.Core/Query/Online/DefaultApiUrls.cs
+++ RailGo.Core/Query/Online/DefaultApiUrls.cs
@@ -9,17 +9,17 @@ public static class DefaultApiUrls
 {
     private static readonly Dictionary<string, string> _urlMappings = new Dictionary<string, string>
     {
-        { "TrainPreselect", "https://data.railgo.zenglingkun.cn/api/train/preselect" },
-        { "TrainQuery", "https://data.railgo.zenglingkun.cn/api/train/query" },
-        { "StationToStationQuery", "https://data.railgo.zenglingkun.cn/api/train/sts_query" },
-        { "StationPreselect", "https://data.railgo.zenglingkun.cn/api/station/preselect" },
-        { "StationQuery", "https://data.railgo.zenglingkun.cn/api/station/query" },
-        { "EmuAssignmentQuery", "https://delay.data.railgo.zenglingkun.cn/api/trainAssignment/queryEmu" },
-        { "TrainDelayQuery", "https://delay.data.railgo.zenglingkun.cn/api/trainDetails/queryTrainDelayDetails" },
-        { "PlatformInfoQuery", "https://mobile.12306.cn/wxxcx/wechat/bigScreen/getExit" },
-        { "GetBigScreenData", "https://screen.data.railgo.zenglingkun.cn" },
-        { "EmuQuery", "https://emu.data.railgo.zenglingkun.cn" },
-        { "DownloadEmuImage", "https://tp.railgo.zenglingkun.cn/api" }
+        { "QueryTrainPreselect", "https://data.railgo.zenglingkun.cn/api/train/preselect" },
+        { "QueryTrainQuery", "https://data.railgo.zenglingkun.cn/api/train/query" },
+        { "QueryStationToStationQuery", "https://data.railgo.zenglingkun.cn/api/train/sts_query" },
+        { "QueryStationPreselect", "https://data.railgo.zenglingkun.cn/api/station/preselect" },
+        { "QueryStationQuery", "https://data.railgo.zenglingkun.cn/api/station/query" },
+        { "QueryEmuAssignmentQuery", "https://delay.data.railgo.zenglingkun.cn/api/trainAssignment/queryEmu" },
+        { "QueryTrainDelay", "https://delay.data.railgo.zenglingkun.cn/api/trainDetails/queryTrainDelayDetails" },
+        { "QueryPlatformInfo", "https://mobile.12306.cn/wxxcx/wechat/bigScreen/getExit" },
+        { "QueryGetBigScreenData", "https://screen.data.railgo.zenglingkun.cn" },
+        { "QueryEmuQuery", "https://emu.data.railgo.zenglingkun.cn" },
+        { "QueryDownloadEmuImage", "https://tp.railgo.zenglingkun.cn/api" }
     };
 
     public static string GetDefaultUrl(string methodName)
